9991a7534c48fc5b250b4ad5c69644af372eece6
andrewaustin12 <andrew.d.austin2@gmail.com>
2023-02-22 18:20:47 -0500

    created app.

---
+++ Socialcademy/Socialcademy/SocialcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialcademyApp.swift
+//  Socialcademy
+//
+//  Created by andrew austin on 2/22/23.
+//
+
+import SwiftUI
+
+@main
+struct SocialcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
