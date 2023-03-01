c289b18a8cf4744c77d09ea6588b8ee6c996dc67
andrewaustin12 <andrew.d.austin2@gmail.com>
2023-02-28 19:34:10 -0500

    Addedd another view where users can revisit posts they’ve favorited before.

---
+++ Socialcademy/Socialcademy/Views/MainTabView.swift
@@ -0,0 +1,29 @@
+//
+//  MainTab.swift
+//  Socialcademy
+//
+//  Created by andrew austin on 2/28/23.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        TabView {
+            PostsList()
+                .tabItem {
+                    Label("Posts", systemImage: "list.dash")
+                }
+            PostsList(viewModel: PostsViewModel(filter: .favorites))
+                .tabItem {
+                    Label("Favorites", systemImage: "heart")
+                }
+        }
+    }
+}
+
+struct MainTab_Previews: PreviewProvider {
+    static var previews: some View {
+        MainTabView()
+    }
+}
